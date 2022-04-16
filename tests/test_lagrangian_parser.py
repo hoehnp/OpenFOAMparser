58e15432692183e6198ef899347d4b60d7ac9fb7
jvwilliams23 <jw144@hw.ac.uk>
2022-04-16 13:27:18 +0100

    add test for max_num_p

--- tests/test_lagrangian_parser.py
+++ tests/test_lagrangian_parser.py
@@ -1,6 +1,7 @@
 import unittest
 from OpenFOAMparser import lagrangian_parser as lp
 
+
 class TestSum(unittest.TestCase):
   def test_lagrangian_parser(self):
     """
@@ -9,7 +10,18 @@ class TestSum(unittest.TestCase):
     """
     test_data = "tests/test_data/lagrangian_U"
     velocity = lp.parse_lagrangian_field(test_data)
-    self.assertEqual(velocity.shape, (1000,3))
+    self.assertEqual(velocity.shape, (1000, 3))
+
+  def test_lagrangian_parser_max_num_p(self):
+    """
+    Verify that the particle velocity vector is read correctly as
+    a numpy array with 10 rows and three columns, when max number 
+    of particles to read is equal to 10
+    """
+    test_data = "tests/test_data/lagrangian_U"
+    velocity = lp.parse_lagrangian_field(test_data, max_num_p=10)
+    self.assertEqual(velocity.shape, (10, 3))
+
 
 if __name__ == "__main__":
   unittest.main()
